b29eda6ddf6454d7aef7b21a0d69927267f5efb3
Prxsphy <132278868+Prxsphy@users.noreply.github.com>
2023-06-15 02:38:41 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const music = new Audio();
 const songs = [
     {
         path: 'assets/1.mp3',
-        displayName: 'Dime Que Me Odias',
+        displayName: 'Euphoria',
         cover: 'assets/1.jpg',
-        artist: 'Depresión Sonora',
+        artist: 'Late night drive home',
     },
 ]
 
